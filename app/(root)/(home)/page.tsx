4e641a1bd0908deb1173b15ca764d04f961be7ed
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-01-13 21:56:19 +0630

    feat(auth): auth feat integrated

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div className="h-screen">
+      <h1>Hola</h1>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+}
