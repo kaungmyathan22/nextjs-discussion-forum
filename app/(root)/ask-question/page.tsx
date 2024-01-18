c9bce4832d0ca279fd2a82a07dea35e35e271385
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-01-18 21:15:38 +0630

    feat(): auth guard added

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,10 +1,18 @@
 import Question from "@/components/forms/Question";
+import { getUserById } from "@/lib/actions/user.action";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const page = () => {
+const page = async () => {
+  const { userId } = auth();
+  if (!userId) redirect("/sign-in");
+  const mongoUser = await getUserById({ userId });
+
   return (
     <div>
-      <div>
-        <Question />
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+      <div className="mt-9">
+        {mongoUser && <Question mongoUserId={JSON.stringify(mongoUser._id)} />}
       </div>
     </div>
   );
