6279e49bf7e2204ee884e8613eb34ecba06b6d64
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-01-18 21:15:27 +0630

    update(): create question action added

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -1,41 +1,45 @@
 "use server";
 
+import Question from "@/database/question.model";
+import Tag from "@/database/tag.model";
 import { connectToDatabase } from "../mongoose";
+import { revalidatePath } from "next/cache";
+
 export const createQuestion = async (params: any) => {
   // eslint-disable-next-line no-empty
   try {
     // connect to DATABASE
     connectToDatabase();
-    // const { title, content, tags, author, path } = params;
+    const { title, content, tags, author, path } = params;
 
     // // create a new question
-    // const question = await Question.create({
-    //   title,
-    //   content,
-    //   author,
-    // });
+    const question = await Question.create({
+      title,
+      content,
+      author,
+    });
 
     // // tag documents array
-    // const tagDocuments = [];
+    const tagDocuments = [];
 
     // create tags or get them if they already exist
-    // for (const tag of tags) {
-    //   const existingTag = await Tag.findOneAndUpdate(
-    //     {
-    //       name: {
-    //         $regex: new RegExp(`^${tag}$`, "i"),
-    //       },
-    //     },
-    //     { $setOnInsert: { name: tag }, $push: { questions: question._id } },
-    //     { upsert: true, new: true },
-    //   );
+    for (const tag of tags) {
+      const existingTag = await Tag.findOneAndUpdate(
+        {
+          name: {
+            $regex: new RegExp(`^${tag}$`, "i"),
+          },
+        },
+        { $setOnInsert: { name: tag }, $push: { questions: question._id } },
+        { upsert: true, new: true },
+      );
 
-    //   tagDocuments.push(existingTag._id);
-    // }
+      tagDocuments.push(existingTag._id);
+    }
 
-    // await Question.findByIdAndUpdate(question._id, {
-    //   $push: { tags: { $each: tagDocuments } },
-    // });
+    await Question.findByIdAndUpdate(question._id, {
+      $push: { tags: { $each: tagDocuments } },
+    });
 
     // // todo: create a interaction record for the user's ask question action
 
@@ -50,7 +54,7 @@ export const createQuestion = async (params: any) => {
 
     // await User.findByIdAndUpdate(author, { $inc: { reputation: 5 } });
 
-    // revalidatePath(path);
+    revalidatePath(path);
   } catch (error) {
     console.log(error);
     throw error;
