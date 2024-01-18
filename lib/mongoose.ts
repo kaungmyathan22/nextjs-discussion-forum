4f6a8e1ffc8ebccd711e93695295532123492d0b
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-01-18 21:15:08 +0630

    update(): change the way mongoose is configured

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -14,9 +14,7 @@ export const connectToDatabase = async () => {
   }
 
   try {
-    await mongoose.connect(process.env.MONGODB_URL, {
-      dbName: "devOverflow",
-    });
+    await mongoose.connect(process.env.MONGODB_URL);
     isConnected = true;
     console.log("MongoDB is connected");
   } catch (error) {
