3477249906586fdf7a10d96d0c44a0a6544d2bbc
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-01-20 23:51:51 +0630

    update(): centering layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <div className="grid h-screen w-screen place-content-center">
+      {children}
+    </div>
+  );
 };
 
 export default Layout;
