812e611dc11dc69d8b8420a34ca843b27e4e7ce8
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-01-21 12:29:01 +0630

    update(actions): get all tags action added

--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -1,6 +1,8 @@
+import Tag from "@/database/tag.model";
 import User from "@/database/use.model";
+import { FilterQuery } from "mongoose";
 import { connectToDatabase } from "../mongoose";
-import { GetTopInteractedTagsParams } from "./shared.types";
+import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared.types";
 
 export const getTopInteractedTags = async (
   params: GetTopInteractedTagsParams,
@@ -42,6 +44,57 @@ export const getTopInteractedTags = async (
     ];
   } catch (error) {
     console.error("Error fetching top interacted tags:", error);
+    throw error;
+  }
+};
+
+export const getAllTags = async (params: GetAllTagsParams) => {
+  try {
+    connectToDatabase();
+
+    const { searchQuery, filter, page = 1, pageSize = 10 } = params;
+
+    // pagination:
+    const skipAmount = (page - 1) * pageSize;
+
+    const query: FilterQuery<typeof Tag> = {};
+
+    if (searchQuery) {
+      query.$or = [{ name: { $regex: new RegExp(searchQuery, "i") } }];
+    }
+
+    let sortOptions = {};
+
+    switch (filter) {
+      case "popular":
+        sortOptions = { questions: -1 };
+        break;
+      case "old":
+        sortOptions = { createdOn: 1 };
+        break;
+      case "recent":
+        sortOptions = { createdOn: -1 };
+        break;
+      case "name":
+        sortOptions = { name: 1 };
+        break;
+
+      default:
+        break;
+    }
+
+    const tags = await Tag.find(query)
+      .skip(skipAmount)
+      .limit(pageSize)
+      .sort(sortOptions);
+
+    const totalTags = await Tag.countDocuments(query);
+
+    const isNext = totalTags > skipAmount + tags.length;
+
+    return { tags, isNext };
+  } catch (error) {
+    console.log(error);
     throw error;
   }
 };
