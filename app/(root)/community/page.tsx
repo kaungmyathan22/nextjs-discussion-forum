7acd40f4cc9c55ad03c3396f215b75fb913d5513
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-01-21 12:20:29 +0630

    update(community): user list shown

--- app/(root)/community/page.tsx
+++ app/(root)/community/page.tsx
@@ -1,12 +1,21 @@
+import UserCard from "@/components/cards/UserCard";
 import Filter from "@/components/shared/Filter";
 import LocalSearchBar from "@/components/shared/search/LocalSearchBar";
 import { UserFilters } from "@/constants/filters";
+import { getAllUsers } from "@/lib/actions/user.action";
+import { SearchParamsProps } from "@/types";
+import { Link } from "lucide-react";
 
-const page = () => {
+const page = async ({ searchParams }: SearchParamsProps) => {
+  const { users, isNext } = await getAllUsers({
+    searchQuery: searchParams?.q,
+    filter: searchParams?.filter,
+    page: searchParams?.page ? +searchParams?.page : 1,
+  });
+
   return (
-    <div>
+    <>
       <h1 className="h1-bold text-dark100_light900">All Users</h1>
-
       <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
         <LocalSearchBar
           route="/community"
@@ -21,7 +30,19 @@ const page = () => {
           otherClasses="min-h-[56px] sm:min-w-[170px]"
         />
       </div>
-    </div>
+      <section className="mt-12 flex flex-wrap gap-4">
+        {users.length > 0 ? (
+          users.map((user) => <UserCard key={user._id} user={user} />)
+        ) : (
+          <div className="paragraph-regular text-dark200_light800 mx-auto max-w-4xl text-center">
+            <p>No User yet</p>
+            <Link href="/sign-up" className="mt-2 font-bold text-accent-blue">
+              Join to be the first!
+            </Link>
+          </div>
+        )}
+      </section>
+    </>
   );
 };
 
