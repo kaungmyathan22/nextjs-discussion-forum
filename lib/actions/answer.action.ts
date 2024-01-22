ebea35cfc38f61a087f8bca75da44733b7697c1c
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-01-22 20:53:28 +0630

    feat(actions): answer related actions added

--- lib/actions/answer.action.ts
+++ lib/actions/answer.action.ts
@@ -1,10 +1,17 @@
 "use server";
 import Answer from "@/database/answer.model";
+import Interaction from "@/database/interaction.model";
 import Question from "@/database/question.model";
 import User from "@/database/user.model";
 import { revalidatePath } from "next/cache";
 import { connectToDatabase } from "../mongoose";
-import { CreateAnswerParams, GetAnswersParams } from "./shared.types";
+import {
+  AnswerVoteParams,
+  CreateAnswerParams,
+  DeleteAnswerParams,
+  GetAnswersParams,
+} from "./shared.types";
+
 export const createAnswer = async (params: CreateAnswerParams) => {
   try {
     connectToDatabase();
@@ -86,6 +93,113 @@ export const getAnswers = async (params: GetAnswersParams) => {
     const isNext = totalAnswer > skipAmount + answers.length;
 
     return { answers, isNext };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const upvoteAnswer = async (params: AnswerVoteParams) => {
+  try {
+    connectToDatabase();
+
+    const { answerId, userId, hasupVoted, path, hasdownVoted } = params;
+
+    let updateQuery = {};
+    if (hasupVoted) {
+      updateQuery = { $pull: { upvotes: userId } };
+    } else if (hasdownVoted) {
+      updateQuery = {
+        $pull: { downvotes: userId },
+        $push: { upvotes: userId },
+      };
+    } else {
+      updateQuery = { $addToSet: { upvotes: userId } };
+    }
+
+    const answer = await Answer.findByIdAndUpdate(answerId, updateQuery, {
+      new: true,
+    });
+    if (!answer) {
+      throw new Error("No Answer found");
+    }
+
+    //  increment author's reputation +2 points for upvoting an answer
+    await User.findByIdAndUpdate(userId, {
+      $inc: { reputation: hasupVoted ? -2 : 2 },
+    });
+
+    await User.findByIdAndUpdate(answer.author, {
+      $inc: { reputation: hasupVoted ? -10 : 10 },
+    });
+
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const downVoteAnswer = async (params: AnswerVoteParams) => {
+  try {
+    connectToDatabase();
+
+    const { answerId, userId, hasupVoted, path, hasdownVoted } = params;
+
+    let updateQuery = {};
+    if (hasdownVoted) {
+      updateQuery = { $pull: { downvotes: userId } };
+    } else if (hasupVoted) {
+      updateQuery = {
+        $pull: { upvotes: userId },
+        $push: { downvotes: userId },
+      };
+    } else {
+      updateQuery = { $addToSet: { downvotes: userId } };
+    }
+
+    const answer = await Answer.findByIdAndUpdate(answerId, updateQuery, {
+      new: true,
+    });
+    if (!answer) {
+      throw new Error("No answer found");
+    }
+
+    //  decrease author's reputation
+
+    await User.findByIdAndUpdate(userId, {
+      $inc: { reputation: hasdownVoted ? -2 : 2 },
+    });
+
+    await User.findByIdAndUpdate(answer.author, {
+      $inc: { reputation: hasdownVoted ? -10 : 10 },
+    });
+
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const deleteAnswer = async (params: DeleteAnswerParams) => {
+  try {
+    connectToDatabase();
+    const { answerId, path } = params;
+
+    const answer = await Answer.findById(answerId);
+    if (!answer) {
+      throw new Error("No answer found");
+    }
+
+    await answer.deleteOne({ _id: answerId });
+    await Question.updateMany(
+      { _id: answer.question },
+      { $pull: { answers: answerId } },
+    );
+    await Interaction.deleteMany({ answer: answerId });
+
+    revalidatePath(path);
   } catch (error) {
     console.log(error);
     throw error;
