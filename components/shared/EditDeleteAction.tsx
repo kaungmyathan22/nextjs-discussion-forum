7dfba230f3192df9af1a6870301ba7fec7f490c8
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-01-27 21:23:26 +0630

    update(): edit / delete feature added

--- components/shared/EditDeleteAction.tsx
+++ components/shared/EditDeleteAction.tsx
@@ -1,36 +1,39 @@
 "use client";
 
+import { deleteAnswer } from "@/lib/actions/answer.action";
+import { deleteQuestion } from "@/lib/actions/question.action";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import { toast } from "../ui/use-toast";
 
 interface Props {
   type: string;
   itemId: string;
 }
 const EditDeleteAction = ({ type, itemId }: Props) => {
-  //   const pathname = usePathname();
+  const pathname = usePathname();
   const router = useRouter();
   const handleEdit = () => {
     router.push(`/question/edit/${JSON.parse(itemId)}`);
   };
   const handleDelete = async () => {
-    // if (type === "Question") {
-    //   // delete Question
-    //   await deleteQuestion({ questionId: JSON.parse(itemId), path: pathname });
-    //   toast({
-    //     title: ` Question Deleted `,
-    //     variant: "destructive",
-    //     description: "Your question has been deleted successfully",
-    //   });
-    // } else if (type === "Answer") {
-    //   // delete Answer
-    //   await deleteAnswer({ answerId: JSON.parse(itemId), path: pathname });
-    //   toast({
-    //     title: `Answer Deleted `,
-    //     variant: "destructive",
-    //     description: "Your answer has been deleted successfully",
-    //   });
-    // }
+    if (type === "Question") {
+      // delete Question
+      await deleteQuestion({ questionId: JSON.parse(itemId), path: pathname });
+      toast({
+        title: ` Question Deleted `,
+        variant: "destructive",
+        description: "Your question has been deleted successfully",
+      });
+    } else if (type === "Answer") {
+      // delete Answer
+      await deleteAnswer({ answerId: JSON.parse(itemId), path: pathname });
+      toast({
+        title: `Answer Deleted `,
+        variant: "destructive",
+        description: "Your answer has been deleted successfully",
+      });
+    }
   };
   return (
     <div className="flex items-center justify-end gap-3 max-sm:w-full">
